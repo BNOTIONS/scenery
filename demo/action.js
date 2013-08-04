f79760bb3aa6cec33333fcc3777068e8ea95bcfd
Mark Reale <mark@reale.ca>
2013-08-03 21:17:15 -0400

    changed DOM element being selected in action file for demo

--- demo/action.js
+++ demo/action.js
@@ -3,19 +3,19 @@ $(document).ready(function(){
 		scenes: 7, /*Number of Scenes || int */
 		scene_length: 'window', /* Height of each scene || int or 'window' if you want the 'length of one of your secenes to be the size of your browser window'  */
 		stage_ele: '#stage', /* element to use as your movie 'stage' */
-		scene_mgr: '#stage' /* element that will track which 'scene' is current in order to apply CSS changes / animations */
+		scene_mgr: 'body' /* element that will track which 'scene' is current in order to apply CSS changes / animations */
 	});
 	
 	var thread = new Thread({
 		element: '#block_1', // DOM Element to animate
 		property: 'top', // CSS Property to adjust
-		prop_start: -50, // Starting Position of CSS property
-		prop_end: 0, // End Position of CSS property
+		prop_start: -50, // Starting Value of CSS property
+		prop_end: 0, // End Value of CSS property
 		unit: '%', // CSS Unit of measurement - use '' if the value does not require a unit
 		timeline_start: 0, // Position on timeline to begin animation (%)
 		timeline_end: 100 // Position on timeline to end animation (%)
 	});
-	var thread = new Thread({
+	var thread2 = new Thread({
 		element: '#block_2',
 		property: 'top', 
 		prop_start: -50, 
@@ -24,7 +24,7 @@ $(document).ready(function(){
 		timeline_start: 0,
 		timeline_end: 100
 	});
-	var thread = new Thread({
+	var thread3 = new Thread({
 		element: '#block_3', 
 		property: 'bottom', 
 		prop_start: -50,
@@ -33,7 +33,7 @@ $(document).ready(function(){
 		timeline_start: 0, 
 		timeline_end: 100 
 	});
-	var thread = new Thread({
+	var thread4 = new Thread({
 		element: '#block_4', 
 		property: 'bottom', 
 		prop_start: -50, 
@@ -42,7 +42,7 @@ $(document).ready(function(){
 		timeline_start: 0,
 		timeline_end: 100 
 	});	
-	var thread = new Thread({
+	var thread5 = new Thread({
 		element: '#block_1', 
 		property: 'left', 
 		prop_start: -50,
@@ -51,7 +51,7 @@ $(document).ready(function(){
 		timeline_start: 0, 
 		timeline_end: 100
 	});
-	var thread = new Thread({
+	var thread6 = new Thread({
 		element: '#block_2',
 		property: 'right',
 		prop_start: -50, 
@@ -60,7 +60,7 @@ $(document).ready(function(){
 		timeline_start: 0, 
 		timeline_end: 100 
 	});
-	var thread = new Thread({
+	var thread7 = new Thread({
 		element: '#block_3', 
 		property: 'left', 
 		prop_start: -50,
@@ -69,7 +69,7 @@ $(document).ready(function(){
 		timeline_start: 0,
 		timeline_end: 100 
 	});
-	var thread = new Thread({
+	var thread8 = new Thread({
 		element: '#block_4',
 		property: 'right', 
 		prop_start: -50, 
@@ -78,7 +78,7 @@ $(document).ready(function(){
 		timeline_start: 0, 
 		timeline_end: 100 
 	});
-	var thread = new Thread({
+	var thread9 = new Thread({
 		element: '.block', 
 		property: 'height', 
 		prop_start: 50,
@@ -87,7 +87,7 @@ $(document).ready(function(){
 		timeline_start: 0, 
 		timeline_end: 100
 	});
-	var thread = new Thread({
+	var thread10 = new Thread({
 		element: '.block',
 		property: 'width',
 		prop_start: 50, 
