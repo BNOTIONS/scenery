394cf5d92f94a029c40e58578f03d4df3f8347e3
Mark Reale <mark@reale.ca>
2013-08-18 15:50:22 -0400

    inclusion of scenery.js site, some other HTML adjustments

--- demo/scenery_website/action.js
+++ demo/scenery_website/action.js
@@ -33,4 +33,54 @@ $(document).ready(function(){
 		timeline_start: '80',
 		timeline_end: '95'
 	});
+
+	var thread3 = new Thread({
+		element: '#tsb p',
+		property: 'top',
+		prop_start: '0',
+		prop_end: '200',
+		unit: 'px',
+		timeline_start: '10',
+		timeline_end: '40'
+	});
+
+	var thread4 = new Thread({
+		element: '#tsb',
+		property: 'opacity',
+		prop_start: '0',
+		prop_end: '1',
+		unit: '',
+		timeline_start: '10',
+		timeline_end: '20'
+	});
+
+	var thread5 = new Thread({
+		element: '#rect-1',
+		property: 'width',
+		prop_start: '0',
+		prop_end: '50',
+		unit: '%',
+		timeline_start: '10',
+		timeline_end: '60'
+	});
+
+	var thread6 = new Thread({
+		element: '#rect-2',
+		property: 'height',
+		prop_start: '0',
+		prop_end: '50',
+		unit: '%',
+		timeline_start: '60',
+		timeline_end: '80'
+	});
+
+	var thread7 = new Thread({
+		element: '#main_content h2',
+		property: 'opacity',
+		prop_start: '0',
+		prop_end: '1',
+		unit: '',
+		timeline_start: '80',
+		timeline_end: '90'
+	});
 });
